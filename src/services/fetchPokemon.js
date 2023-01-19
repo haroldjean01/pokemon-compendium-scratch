fa650cb10b307a310669281821523081334230bf
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-19 12:31:54 -0800

    fixed semicolon issues

--- src/services/fetchPokemon.js
+++ src/services/fetchPokemon.js
@@ -1,6 +1,6 @@
 export async function fetchInitialPokemon(){
-    const resp = await fetch('https://alchemy-pokedex.herokuapp.com/api/pokedex/');
-    const data = resp.json();
-    console.log('data', data);
-    return data.results;
+  const resp = await fetch('https://alchemy-pokedex.herokuapp.com/api/pokedex/');
+  const data = resp.json();
+  console.log('data', data);
+  return data.results;
 }
