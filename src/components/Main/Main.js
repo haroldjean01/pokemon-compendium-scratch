63682f9db0f8fb6b085f356a1c2ac9cae1c40008
Harold Jean <haroldjean38@yahoo.com>
2023-01-19 18:41:32 -0500

    added Select function to Main having syntax error will correct

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { usePokemon } from '../../hooks/usePokemon.js';
 import PokeCard from '../PokeCard/PokeCard.js';
+import Select from '../Select/Select.js';
 import './Main.css';
 
 export default function Main() {
   const { types, pokemon } = usePokemon();
 
   return (
-    <div className="poke-card-container">
-      {pokemon.map((poke) => (
-        <PokeCard key={poke.id} {...poke} />
-      ))}
-    </div>
+    <>
+      <Select types={types} />
+      <div className="poke-card-container">
+        {pokemon.map((poke) => (
+          <PokeCard key={poke.id} {...poke} />
+        ))}
+      </div>
+    </>
   );
 }
---
+++ src/components/Select/Select.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Select({ types }) {
+  return (
+    <select>
+      {types.map(({ type }) => (
+        <option key={type} value={type}>
+          {type}
+        </option>
+      ))}
+    </select>
+  );
+}
