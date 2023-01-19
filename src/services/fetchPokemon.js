aadf00ed4ee6459ef9a9f255bc505199334fd6d3
Harold Jean <haroldjean38@yahoo.com>
2023-01-19 15:20:23 -0500

    added pokedex link to fetch file

--- src/services/fetchPokemon.js
+++ src/services/fetchPokemon.js
@@ -1,3 +1,6 @@
 export async function fetchInitialPokemon(){
-    const resp = await fetch('')
+    const resp = await fetch('https://alchemy-pokedex.herokuapp.com/api/pokedex/');
+    const data = resp.json();
+    console.log('data', data);
+    return data.results;
 }
