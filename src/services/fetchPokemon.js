043c070fa257e47bde741e217ca4b296e47ef75d
Harold Jean <haroldjean38@yahoo.com>
2023-01-19 18:00:05 -0500

    installed fetchPokemonTypes into fetchPokemon file

--- src/services/fetchPokemon.js
+++ src/services/fetchPokemon.js
@@ -3,3 +3,8 @@ export async function fetchInitialPokemon() {
   const data = await resp.json();
   return data.results;
 }
+export async function fetchPokemonTypes() {
+  const resp = await fetch('https://alchemy-pokedex.herokuapp.com/api/pokedex/types');
+  const data = await resp.json();
+  return data.results;
+}
