682a243b58a1a2bd357c062a21393258f666a925
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-19 14:17:00 -0800

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header/Header.js';
 import Main from './components/Main/Main.js';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return <header>
+    <img></img>
+    <h1>React App</h1>
+    <div className='fixer'></div>
+  </header>;
+}
