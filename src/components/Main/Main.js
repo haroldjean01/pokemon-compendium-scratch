387bd81eb9c7cfcda6d9027011f8382c10f40001
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-19 17:38:42 -0800

    added background image and more styling

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,18 +3,19 @@ import { usePokemon } from '../../hooks/usePokemon.js';
 import PokeCard from '../PokeCard/PokeCard.js';
 import Select from '../Select/Select.js';
 import './Main.css';
+import background from './background.jpg';
 
 export default function Main() {
   const { pokemon, types, handleTypeChange, loading } = usePokemon();
 
   return (
-    <>
+    <div style={{ backgroundImage: `url(${background})`, backgroundSize: '50%', backgroundRepeat: 'repeat' }}>
       <Select types={types} handleTypeChange={handleTypeChange} />
       <div className="poke-card-container">
         {pokemon.map((poke) => (
           <PokeCard key={poke.id} {...poke} loading={loading} />
         ))}
       </div>
-    </>
+    </div>
   );
 }
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -3,12 +3,15 @@ import './Select.css';
 
 export default function Select({ types, handleTypeChange }) {
   return (
-    <select onChange={(e) => handleTypeChange(e.target.value)}>
-      {types.map(({ type }) => (
-        <option key={type} value={type}>
-          {type.toUpperCase()}
-        </option>
-      ))}
-    </select>
+    <>
+      <h3>Filter by Type!</h3>
+      <select onChange={(e) => handleTypeChange(e.target.value)}>
+        {types.map(({ type }) => (
+          <option key={type} value={type}>
+            {type.toUpperCase()}
+          </option>
+        ))}
+      </select>
+    </>
   );
 }
