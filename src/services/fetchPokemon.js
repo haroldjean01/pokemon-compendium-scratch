ce7032c619f69160bd79bef8f20dbcc748b2fd27
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-19 16:07:04 -0800

    added fetchPokemon by type in fetch file

--- src/services/fetchPokemon.js
+++ src/services/fetchPokemon.js
@@ -3,8 +3,22 @@ export async function fetchInitialPokemon() {
   const data = await resp.json();
   return data.results;
 }
+
 export async function fetchPokemonTypes() {
   const resp = await fetch('https://alchemy-pokedex.herokuapp.com/api/pokedex/types');
   const data = await resp.json();
   return data;
+}
+
+export async function fetchPokemon(type) {
+  const params = new URLSearchParams();
+  params.set('type', type);
+
+  const response = await fetch(
+    `https://alchemy-pokedex.herokuapp.com/api/pokedex/?${params.toString()}`
+  );
+
+  const data = await response.json();
+
+  return data.results;
 }
